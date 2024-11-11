8f8c4ffc17c9809024c903f403ea9ee7faf7137c
Arslan Majeed <arslanmajeed214@gmail.com>
2024-11-11 15:27:40 +0500

    Implement the search functionality

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
-import { Genre } from "./useGenre";
 
 interface FetchResponse<T> {
   count: number;
