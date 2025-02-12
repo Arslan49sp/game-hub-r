662059be00b9bb6d09ca4d924c8c4b8743343e20
Arslan Majeed <arslanmajeed214@gmail.com>
2025-02-12 18:59:34 +0500

    setting the zustand store for gameQuery

---
+++ src/store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+interface GameQuery {
+  genreId?: number;
+  platformId?: number;
+  sortOrder?: string;
+  searchText?: string;
+}
+
+export interface GameQueryStore {
+  gameQuery: GameQuery;
+  setSearchText: (searchText: string) => void;
+  setGenreId: (genreId: number) => void;
+  setPlatformId: (platormId: number) => void;
+  setSortOrder: (sortOrder: string) => void;
+}
+
+create<GameQueryStore>((set) => ({
+  gameQuery: {},
+  setSearchText: (searchText) => set(() => ({ gameQuery: { searchText } })),
+  setGenreId: (genreId) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, genreId } })),
+  setPlatformId: (platformId) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, platformId } })),
+  setSortOrder: (sortOrder) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, sortOrder } })),
+}));
