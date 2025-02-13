b3a29239f110971b9f1773d5697cd8e5950a6f82
Arslan Majeed <arslanmajeed214@gmail.com>
2025-02-13 17:24:45 +0500

    Refactor hooks to extract interfaces entities.

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,14 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { genres } from "../data/genras";
 import APIClient from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
-
 const useGenres = () => {
   return useQuery({
     queryKey: ["genras"],
